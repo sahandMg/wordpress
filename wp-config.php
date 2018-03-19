51446eb910aa82f9ad6bde2bf6df0ebbba347044
sahandMg <sahand.mg.ne@gmail.com>
2018-03-19 12:03:29 +0330

    db changed

--- wp-config.php
+++ wp-config.php
@@ -29,10 +29,11 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+// define('DB_HOST', 'localhost');
+define('DB_HOST', 'ec2-54-243-238-144.compute-1.amazonaws.com:5432');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
